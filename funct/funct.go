1f0e0bea0c37b08cc0bd4baad74c01b24eab8208
Raphael Reyna <raphaelreyna@protonmail.com>
2020-01-25 19:43:32 -0800

    Add retry func configuration struct

--- funct/funct.go
+++ funct/funct.go
@@ -9,6 +9,7 @@ type FailableNullaryFunc func() error
 type FloatSequence func(uint) float32
 type ErrorHandler func(error)
 
+// DefaultBackoffSequence waits 1 second for first 4 seconds, and then increases wait time by 1.5 seconds per attempt
 var DefaultBackoffSequence FloatSequence = func(a uint) float32 {
 	if a < 4 {
 		return 1.0
@@ -16,31 +17,49 @@ var DefaultBackoffSequence FloatSequence = func(a uint) float32 {
 	return 1.5*float32(a) - 4.0
 }
 
+// DefaultErrorHandler logs the error
 var DefaultErrorHandler ErrorHandler = func(err error) {
 	log.Println(err)
 }
 
-// Retry keeps calling `f()` until it returns a nil error or number of attempts exceeds `attemmpts`.
-// If after each call to `f()`, there is an error, then its passed to errorHandler.
-// The `errorHandler` function will be called as a go routine each time.
-// The parameter `waitMap` should map the current attempt number to number of seconds to wait before next attempt.
-// The first time `waitMap` is called will be during attempt 2.
-func (f FailableNullaryFunc) Retry(attempts uint, waitMap FloatSequence, errorHandler ErrorHandler) error {
+type RetryConf struct {
+	Retries uint
+	// WaitMap should map the current attempt number to number of seconds to wait before next attempt.
+	// The first time WaitMap is called will be during attempt 2.
+	WaitMap FloatSequence
+	// ErrorHandler will be called as a go routine each time.
+	ErrHandler           ErrorHandler
+	ConcurrentErrHandler bool
+}
+
+// Retry keeps calling `f()` until it returns a nil error or number of retries is exceeded.
+// If after each call to `f()`, there is an error, then its passed to an error handler.
+func (f FailableNullaryFunc) Retry(conf *RetryConf) error {
+	retries := conf.Retries
+	errHandler := conf.ErrHandler
+	if errHandler == nil {
+		errHandler = DefaultErrorHandler
+	}
+	wm := conf.WaitMap
+	if wm == nil {
+		wm = DefaultBackoffSequence
+	}
+	concerr := conf.ConcurrentErrHandler
 	// Initiliaze attemp counter and error
-	err := f.Call(errorHandler, true)
+	err := f.Call(errHandler, concerr)
 	var attempt uint = 2
 	// Loop until success or attempts run out
 	for err != nil {
 		// Make sure we can still make an attempt
-		if attempt >= attempts {
+		if attempt >= conf.Retries {
 			// If out of attempts, return last attempts error
 			return err
 		}
 		// Wait based on func w
-		waitTime := time.Duration(waitMap(attempt)) * time.Second
+		waitTime := time.Duration(wm(retries)) * time.Second
 		time.Sleep(waitTime)
 		// Call function
-		err = f.Call(errorHandler, true)
+		err = f.Call(errHandler, true)
 		// Increase attempt count
 		attempt = attempt + 1
 	}
