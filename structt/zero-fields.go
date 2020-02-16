4bb65cc91bfd34bc31a2646dd46fa020a13fbe15
Raphael Reyna <raphaelreyna@protonmail.com>
2020-02-15 22:36:24 -0800

    Add func to detect zero valued fields

--- structt/zero-fields.go
+++ structt/zero-fields.go
@@ -3,6 +3,7 @@ package structt
 import "reflect"
 
 // ZeroFields zeroes out the values for the passed in field names.
+// Panics if i is not a pointer to a struct.
 func ZeroFields(i interface{}, fields... string) {
 	iv := reflect.ValueOf(i).Elem()
 	for _, fn := range fields {
@@ -15,4 +16,32 @@ func ZeroFields(i interface{}, fields... string) {
 			df.Set(zero)
 		}
 	}
+}
+
+// HasZeroes returns true if i has at least one field with a zero value.
+// Ignores zero value cases for field names given as exceptions.
+// Panics if i is not a struct or pointer to a struct.
+func HasZeroes(i interface{}, exceptions... string) bool {
+	iv := reflect.ValueOf(i)
+	if iv.Kind() == reflect.Ptr {
+		iv = iv.Elem()
+	}
+	it := iv.Type()
+	for j := 0 ; j < it.NumField() ; j++ {
+		fv := iv.Field(j)
+		if fv.IsZero() {
+			fn := it.Field(j).Name
+			var exception bool
+			for _, e := range exceptions {
+				if fn == e {
+					exception = true
+					break
+				}
+			}
+			if !exception {
+				return true
+			}
+		}
+	}
+	return false
 }
--- structt/zero-fields_test.go
+++ structt/zero-fields_test.go
@@ -3,12 +3,11 @@ package structt
 import "testing"
 
 func TestZeroFields(t *testing.T) {
-	type A struct {
+	a := struct {
 		First string
 		Second string
 		Third int
-	}
-	a := A{
+	}{
 		First: "firstvalue",
 		Second: "secondvalue",
 		Third: 3,
@@ -24,3 +23,110 @@ func TestZeroFields(t *testing.T) {
 		t.Errorf(`third field: %d != 0`, a.Third)
 	}
 }
+
+func TestHasZeroes(t *testing.T) {
+	type A struct {
+		First string
+		Second int
+		Third bool
+		Fourth string
+	}
+	type test struct {
+		description string
+		a A
+		exceptions []string
+		want bool
+	}
+	tt := []test{
+		test{
+			description: "no zeroes, no exceptions",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+				Fourth: "fourthvalue",
+			},
+			want: false,
+		},
+		test{
+			description: "has zero, no exceptions",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+			},
+			want: true,
+		},
+		test{
+			description: "no zeroes, has exception",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+				Fourth: "fourthvalue",
+			},
+			exceptions: []string{
+				"Fourth",
+			},
+			want: false,
+		},
+		test{
+			description: "has zero and exception",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+			},
+			exceptions: []string{
+				"Fourth",
+			},
+			want: false,
+		},
+		test{
+			description: "no zeroes, exceptions has non existing field name",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+				Fourth: "fourthvalue",
+			},
+			exceptions: []string{
+				"Fifth",
+			},
+			want: false,
+		},
+		test{
+			description: "has zero, exceptions has non existing field name",
+			a: A{
+				First: "firstvalue",
+				Second: 5,
+				Third: true,
+			},
+			exceptions: []string{
+				"Fourth",
+				"Fifth",
+			},
+			want: false,
+		},
+		test{
+			description: "has zeroes and exception",
+			a: A{
+				First: "firstvalue",
+				Third: true,
+			},
+			exceptions: []string{
+				"Fourth",
+			},
+			want: true,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.description, func(t *testing.T) {
+			hz := HasZeroes(tc.a, tc.exceptions...)
+			if hz != tc.want {
+				t.Errorf("got: %v; expected: %v", hz, tc.want)
+			}
+		})
+	}
+}
+
